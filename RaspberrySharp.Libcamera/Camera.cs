a170ac3bcd70d57fd98a5a324ebdfd46b076e9e6
Luna <dev@lunardiver.com>
2021-12-11 22:39:18 +0100

    Added basic method to Camera class.

--- RaspberrySharp.Libcamera/Camera.cs
+++ RaspberrySharp.Libcamera/Camera.cs
@@ -2,5 +2,11 @@
 
 public class Camera
 {
-
+    public byte[] TakeJpegImage()
+    {
+        using var proc = new CameraProcess("-t 1 -n -o - --width 640 --height 480");
+        proc.Start();
+        proc.WaitForExit((int)TimeSpan.FromSeconds(5).TotalMilliseconds);
+        return proc.GetStandardData();
+    }
 }
